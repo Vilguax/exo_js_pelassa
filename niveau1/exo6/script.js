c2631a4c98075f4d26c8bf086ab4084a3807294d
Vilguax <axel.pelassa@hotmail.fr>
2023-07-05 14:39:28 +0200

    Niveau 1 exo 6

---
+++ niveau1/exo6/script.js
@@ -0,0 +1,4 @@
+var num = prompt("Entrez un nombre pour savoir si il est pair ou impaire :");
+var result = num % 2;
+if (result == 0) {console.log("Le nombre est pair");}
+else {console.log("Le nombre est impaire");}
