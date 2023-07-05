3bc8466a6a64884ea321e42451f7abd9eef4a04f
Vilguax <axel.pelassa@hotmail.fr>
2023-07-05 16:33:57 +0200

    Niveau 3 exo 7

---
+++ niveau3/exo7/script.js
@@ -0,0 +1,12 @@
+function evaluerExpression(expression) {
+    var resultat = eval(expression);
+    if (isFinite(resultat)) {
+        return resultat;
+      } else {
+        return "Erreur: Expression invalide";
+      }
+  }
+
+var expression = "2 + 3 * 4";
+var resultat = evaluerExpression(expression);
+console.log(resultat);
