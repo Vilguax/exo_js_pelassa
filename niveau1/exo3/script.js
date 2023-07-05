9b4096f111813810190bbf507935becd4d226a0e
Vilguax <axel.pelassa@hotmail.fr>
2023-07-05 14:22:56 +0200

    Niveau 1 exo 3

---
+++ niveau1/exo3/script.js
@@ -0,0 +1,6 @@
+var num1 = prompt("Entrez le premier nombre :")
+var num2 = prompt("Entrez le deuxième nombre :")
+
+var result = parseInt(num1) + parseInt(num2)
+
+console.log("Le résultat de l'addition est : " + result)
